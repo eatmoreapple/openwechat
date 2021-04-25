67cff85503325ed2a5f5bb1c8b709cf2b5eba871
eatMoreApple <15055461510@163.com>
2021-04-25 11:28:58 +0800

    添加emoji表情支持

--- emoji_test.go
+++ emoji_test.go
@@ -5,3 +5,17 @@ import "testing"
 func TestFormatEmoji(t *testing.T) {
 	t.Log(FormatEmoji(`多吃点苹果<span class="emoji emoji1f34f"></span>`))
 }
+
+func TestSendEmoji(t *testing.T) {
+	self, err := getSelf()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	f, err := self.FileHelper()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_ = f.SendText(Emoji.Dagger)
+}
