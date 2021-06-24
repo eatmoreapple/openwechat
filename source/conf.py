caaba3c332d24e831b564c7f032e77867e906bd9
eatMoreApple <15055461510@163.com>
2021-06-24 10:29:17 +0800

    update conf.py

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,7 @@ release = 'v1'
 # ones.
 extensions = [
     'recommonmark',
-    'sphinx_markdown_tables'
+    # 'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
