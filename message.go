8b584b537afcaf3968f7ad52e2dcffb8773643fd
eatmoreapple <15055461510@163.com>
2021-08-03 17:54:59 +0800

    修复HasFile :bug:

--- message.go
+++ message.go
@@ -21,7 +21,7 @@ type Message struct {
 		Type  int
 		AppID string
 	}
-	AppMsgType            int
+	AppMsgType            AppMessageType
 	HasProductId          int
 	ImgHeight             int
 	ImgStatus             int
@@ -229,7 +229,7 @@ func (m *Message) StatusNotify() bool {
 
 // HasFile 判断消息是否为文件类型的消息
 func (m *Message) HasFile() bool {
-	return m.IsPicture() || m.IsVoice() || m.IsVideo() || m.IsMedia() || m.IsEmoticon()
+	return m.IsPicture() || m.IsVoice() || m.IsVideo() || (m.IsMedia() && m.AppMsgType == AppMsgTypeAttach) || m.IsEmoticon()
 }
 
 // GetFile 获取文件消息的文件
