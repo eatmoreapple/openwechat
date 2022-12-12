270e29047249c58c5364e376fd64c562e048068c
多吃点苹果 <73388495+eatmoreapple@users.noreply.github.com>
2022-12-12 11:55:55 +0800

    修改热登录逻辑 (#155)

--- errors.go
+++ errors.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 )
 
-var NetworkErr = errors.New("wechat network error")
-
 func IsNetworkError(err error) bool {
 	return errors.Is(err, NetworkErr)
 }
@@ -19,5 +17,18 @@ func IgnoreNetworkError(errHandler func(err error)) func(error) {
 	}
 }
 
-// ErrForbidden 禁止当前账号登录
-var ErrForbidden = errors.New("login forbidden")
+var (
+	// ErrForbidden 禁止当前账号登录
+	ErrForbidden = errors.New("login forbidden")
+
+	// ErrInvalidStorage define invalid storage error
+	ErrInvalidStorage = errors.New("invalid storage")
+
+	// NetworkErr define wechat network error
+	NetworkErr = errors.New("wechat network error")
+)
+
+// Error impl error interface
+func (r Ret) Error() string {
+	return r.String()
+}
