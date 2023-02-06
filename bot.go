1f7b8f28b655ec0948ae9a64b864dfff7db60657
wenyoufu <wenyoufu@jd.com>
2023-02-06 20:48:10 +0800

    【bug-fix】修改不在通讯录无法获取群组的bug 解决方案：程序运行后，如果群组（不在通讯录中）,发送过任意消息，则更新群组

--- bot.go
+++ bot.go
@@ -202,6 +202,27 @@ func (b *Bot) WebInit() error {
 	return nil
 }
 
+func (b *Bot) updateGroups(msg *Message) {
+	if msg.IsSendByGroup() {
+		if msg.FromUserName == msg.bot.self.User.UserName {
+			return
+		}
+		// 首先尝试从缓存里面查找, 如果没有找到则从服务器获取
+		members, err := msg.bot.self.Members()
+		if err != nil {
+			return
+		}
+		user, exist := members.GetByUserName(msg.FromUserName)
+		if !exist {
+			// 找不到, 从服务器获取
+			user = &User{self: msg.bot.self, UserName: msg.FromUserName}
+			err = user.Detail()
+			b.self.members = b.self.members.Apppend(user)
+			b.self.groups = b.self.members.Groups()
+		}
+	}
+}
+
 // 轮询请求
 // 根据状态码判断是否有新的请求
 func (b *Bot) syncCheck() error {
@@ -241,6 +262,7 @@ func (b *Bot) syncCheck() error {
 				// 如果异步调用则需自行处理
 				// 如配合 openwechat.MessageMatchDispatcher 使用
 				// NOTE: 请确保 MessageHandler 不会阻塞，否则会导致收不到后续的消息
+				b.updateGroups(message)
 				b.MessageHandler(message)
 			}
 		}
